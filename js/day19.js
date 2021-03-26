0966f45272b08060a4487716d17b89050fa78d0d
MaxwellYu <yuichiuyu1915@gmail.com>
2021-03-26 21:31:44 +0800

    26-03-2021

---
+++ js/day19.js
@@ -0,0 +1,11 @@
+navMenu = document.getElementById("navMenu");
+
+function openNav(){
+    navMenu.style.width = "250px";
+    navMenu.style.min_width = "200px";
+}
+
+function closeNav(){
+    navMenu.style.width = "0";
+    navMenu.style.min_width = "0px";
+}
