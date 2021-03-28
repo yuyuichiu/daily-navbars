130dbeb4628a0b5b72c0479ca3c5d489b191ca6b
MaxwellYu <yuichiuyu1915@gmail.com>
2021-03-29 07:30:30 +0800

    28-03-2021 update

--- js/day21.js
+++ js/day21.js
@@ -15,3 +15,37 @@ function closeOverlay(){
     ol.style.width = "0px";
     ol.style.min_width = "0px";
 }
+
+//Random String and Remove Vowel
+var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+function randomStr() {
+    out = document.getElementsByClassName("output");
+    for (i = 0; i < out.length; i++){
+        out[i].innerHTML = "";
+        for (x = 0; x < 30; x++){
+            //Extract a random index to pick a random letter to add into output area
+            out[i].innerText += letters.charAt(Math.floor(Math.random() * letters.length));
+        }
+    }
+}
+
+function removeVowels() {
+    out = document.getElementsByClassName("output");
+
+    for (i = 0; i < out.length; i++) {
+        /* Array Method
+        var arr = out[i].innerText.split("");
+        var pattern = /[^aeiou]/i;
+        var filtered = arr.filter(x => x.match(pattern));
+        }*/
+
+        //String Method
+        var pattern = /[^aeiou]/gi;
+        noVowelResult = out[i].innerText.match(pattern);
+
+        out[i].innerText = "";
+        for (j = 0; j < noVowelResult.length; j++){
+            out[i].innerText += noVowelResult[j];
+        }
+    }
+}
