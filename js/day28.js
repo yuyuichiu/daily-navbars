ab769cf51afab77ffadb4d65019402e0b1186201
MaxwellYu <yuichiuyu1915@gmail.com>
2021-04-05 20:36:14 +0800

    05-04-2021

---
+++ js/day28.js
@@ -0,0 +1,9 @@
+function overlayToggle(){
+    
+}
+
+function validate(){
+    pw = document.getElementById("pw").value;
+    output = document.getElementById("pwOutput");
+    output.innerText = /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[a-zA-Z0-9]{8,}$/.test(pw);
+}
