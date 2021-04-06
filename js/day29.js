dbae61bf64a8a9d3c34d6ff2fe4bcaf91f768f4b
MaxwellYu <yuichiuyu1915@gmail.com>
2021-04-06 23:49:35 +0800

    06-04-2021

--- js/day29.js
+++ js/day29.js
@@ -1,9 +1,31 @@
-function overlayToggle(){
-    
+function gcd(){
+    num1 = document.getElementById("num1").value;
+    num2 = document.getElementById("num2").value;
+    output = document.getElementById("GCDoutput");
+
+    if (num1 == num2){
+        output.innerText = "Greatest Common Divider is " + num1
+        return
+    }
+
+    rawBig = (num1 < num2) ? num2 : num1;
+    rawSmall = (num1 < num2) ? num1 : num2;
+
+    while (rawBig > 0 && rawSmall > 0){
+        while(rawBig > rawSmall){
+            rawBig = rawBig % rawSmall;
+        }
+        if (rawBig < rawSmall){
+            temp = rawSmall;
+            rawSmall = rawBig;
+            rawBig = temp;
+        }
+    }
+
+    output.innerText = "Greatest Common Divider is " + rawBig;
 }
 
-function validate(){
-    pw = document.getElementById("pw").value;
-    output = document.getElementById("pwOutput");
-    output.innerText = /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[a-zA-Z0-9]{8,}$/.test(pw);
+function show(){
+    document.getElementById("hiddentxt").style.visibility = "visible";
+    document.getElementById("hiddentxt").style.opacity = 1;
 }
